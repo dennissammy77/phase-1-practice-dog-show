81f0012c74af3c054b16bd25e20c362967fe769c
dennissammy77 <dennissammy77@gmail.com>
2025-03-30 13:39:14 +0300

    pratice

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    const dogTableList = document.getElementById("table-body");
+    const dogForm = document.getElementById("dog-form");
+    fetch('http://localhost:3000/dogs').then((response)=>response.json()).then((data)=>{
+        data.map((dog)=>{
+            const tData = document.createElement('tr');
+            tData.innerHTML=`
+                <td>${dog.name}</td>
+                <td>${dog.breed}</td>
+                <td>${dog.sex}</td>
+                <td><button id="dog-${dog.id}">Edit</button></td>
+            `
+            dogTableList.appendChild(tData)
+            document.getElementById(`dog-${dog.id}`).addEventListener("click",()=>{
+                // populate form 
+                dogForm.querySelector('[name=name').value=dog.name
+                dogForm.querySelector('[name=breed').value=dog.breed
+                dogForm.querySelector('[name=sex').value=dog.sex
+            });
+            dogForm.addEventListener('submit',(e)=>{
+                e.preventDefault();
+                const name = dogForm.querySelector('[name=name').value;
+                const breed = dogForm.querySelector('[name=breed').value;
+                const sex = dogForm.querySelector('[name=sex').value;
+                fetch(`http://localhost:3000/dogs/${dog.id}`,{
+                    method: 'PATCH',
+                    headers: {
+                        "content-Type": "application/json",
+                        "accept": "application/json"
+                    },
+                    data: JSON.stringify({
+                        name,
+                        breed,
+                        sex
+                    })
+                }).catch((err)=>console.log(err))
+            })
+        })
+    }).catch((err)=>console.error(err))
 })
